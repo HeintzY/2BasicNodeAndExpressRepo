11ef219cda5f0236ce3b8dae1e61b9dbd7547d83
abctestuser <abctestmail@testmail.com>
2025-04-29 15:17:40 +0200

    feat: Use the .env File
    Create a .env file in the root of your project
    directory, and store the variable
    MESSAGE_STYLE=uppercase in it.
    
    Then, in the /json GET route handler you created
    in the last challenge access
    process.env.MESSAGE_STYLE and transform the
    response object's message to uppercase if the
    variable equals uppercase. The response object
    should either be {"message": "Hello json"} or
    {"message": "HELLO JSON"}, depending on the
    MESSAGE_STYLE value. Note that you must read the
    value of process.env.MESSAGE_STYLE inside the
    route handler, not outside of it, due to the
    way our tests run.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 // Meet the Node console
 // Modify the myApp.js file to log "Hello World" to the console.
@@ -27,15 +28,22 @@ app.use("/public", express.static(__dirname + "/public"));
 
 // Serve JSON on a Specific Route
 // Serve the object {"message": "Hello json"} as a response, in JSON format, to GET requests to the /json route. Then point your browser to your-app-url/json, you should see the message on the screen.
+// app.get("/json", function (req, res) {
+//     res.json({ "message": "Hello json" });
+// });
+
+
+// Use the .env File
+// Create a .env file in the root of your project directory, and store the variable MESSAGE_STYLE=uppercase in it.
+// Then, in the /json GET route handler you created in the last challenge access process.env.MESSAGE_STYLE and transform the response object's message to uppercase if the variable equals uppercase. The response object should either be {"message": "Hello json"} or {"message": "HELLO JSON"}, depending on the MESSAGE_STYLE value. Note that you must read the value of process.env.MESSAGE_STYLE inside the route handler, not outside of it, due to the way our tests run.
 app.get("/json", function (req, res) {
-    res.json({ "message": "Hello json" });
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({ "message": "HELLO JSON" });
+    }
+    else {
+        res.json({ "message": "Hello json" });
+    }
 });
-
-
-
-
-
-
 
 
 
