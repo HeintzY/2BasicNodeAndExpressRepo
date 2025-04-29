0122528f19fe9651d2c848103ab2d6ee94dae5ee
abctestuser <abctestmail@testmail.com>
2025-04-29 14:19:00 +0200

    feat: Start a Working Express Server
    Use the app.get() method to serve the string
    "Hello Express" to GET requests matching
    the / (root) path. Be sure that your code works by
    looking at the logs.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,15 @@
 let express = require('express');
 let app = express();
-console.log("Hello World")
+
+// Meet the Node console
+// Modify the myApp.js file to log "Hello World" to the console.
+console.log("Hello World");
+
+// Start a Working Express Server
+// Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path. Be sure that your code works by looking at the logs.
+app.get("/", function (req, res) {
+    res.send('Hello Express');
+});
 
 
 
