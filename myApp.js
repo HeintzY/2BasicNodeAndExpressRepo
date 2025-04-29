73163515d07d501d66ff2add119e4e41ba27cda6
abctestuser <abctestmail@testmail.com>
2025-04-29 14:27:26 +0200

    feat: Serve an HTML File
    Send the /views/index.html file as a response to
    GET requests to the / path. If you view your live
    app, you should see a big HTML heading
    (and a form that we will use later…), with no
    style applied.

--- myApp.js
+++ myApp.js
@@ -7,13 +7,16 @@ console.log("Hello World");
 
 // Start a Working Express Server
 // Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path. Be sure that your code works by looking at the logs.
+// app.get("/", function (req, res) {
+//     res.send('Hello Express');
+// });
+
+
+// Serve an HTML File
+//Send the /views/index.html file as a response to GET requests to the / path. If you view your live app, you should see a big HTML heading (and a form that we will use later…), with no style applied.
 app.get("/", function (req, res) {
-    res.send('Hello Express');
+    res.sendFile(__dirname + "/views/index.html");
 });
-
-
-
-
 
 
 
