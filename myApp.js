59bc099be516b04ef1224185a1aecb0c626b8262
abctestuser <abctestmail@testmail.com>
2025-04-30 09:55:31 +0200

    feat: Chain Middleware to Create a Time Server
    In the route app.get('/now', ...) chain a
    middleware function and the final handler. In the
    middleware function you should add the current
    time to the request object in the req.time key.
    You can use new Date().toString(). In the handler,
    respond with a JSON object, taking the structure
    {time: req.time}.

--- myApp.js
+++ myApp.js
@@ -60,8 +60,15 @@ app.get("/json", function (req, res) {
 
 
 
-
-
+// 8:
+// Chain Middleware to Create a Time Server
+// In the route app.get('/now', ...) chain a middleware function and the final handler. In the middleware function you should add the current time to the request object in the req.time key. You can use new Date().toString(). In the handler, respond with a JSON object, taking the structure {time: req.time}.
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.json({ "time": req.time })
+});
 
 
 
